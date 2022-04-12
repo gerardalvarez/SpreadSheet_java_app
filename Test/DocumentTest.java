85647054bb43822b16ba6d528537c5a4d3c5ffbd
Gerard <gerardalvariz@gmail.com>
2022-04-12 21:35:00 +0200

    generating tests

---
+++ Test/DocumentTest.java
@@ -0,0 +1,35 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DocumentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void set_nom() {
+    }
+
+    @Test
+    void afegir_full() {
+    }
+
+    @Test
+    void elimina_full() {
+    }
+
+    @Test
+    void setData_ultima_mod() {
+    }
+
+    @Test
+    void getFulls() {
+    }
+
+    @Test
+    void get_full() {
+    }
+}
+
