d90a49448ef76012ee80f1bc67865e483e6b9932
MiguelFrias <miguel.frias@estudiantat.upc.edu>
2022-05-29 17:13:51 +0200

    Comentando Full

--- src/main/CapaDomini/Models/Full.java
+++ src/main/CapaDomini/Models/Full.java
@@ -18,18 +18,16 @@ public class Full {
     private Integer Num_Columnes;
     private Integer Num_Files;
     private HashMap<AbstractMap.SimpleEntry<Integer,Integer>, Cela> Celes;
-    private ArrayList<Accio> Estatsprevis;
 
     //Constructor
     /**
-     * @brief Constructora de Full que crea el full a partird del nom, el número de columnes i el número files.
-     * @param n
-     * @param nc
-     * @param nf
+     * Constructora de Full que crea el full a partir del nom, el numero de columnes i el numero files.
+     * @param n nom del full
+     * @param nc numero de columnes del full
+     * @param nf numero de files del full
      */
     public Full(String n, Integer nc, Integer nf) {
         Celes = new HashMap<>();
-        Estatsprevis= new ArrayList<>();
         this.nom = n;
         this.Num_Columnes = nc;
         this.Num_Files = nf;
@@ -43,9 +41,9 @@ public class Full {
     };
 
     /**
-     * @brief Constructora de Full que crea el full a partird del número de columnes i el número files.
-     * @param nc
-     * @param nf
+     * Constructora de Full que crea el full a partir del numero de columnes i el numero files.
+     * @param nc numero de columnes del full
+     * @param nf numero de files del full
      */
     public Full(Integer nc, Integer nf) {
         this.nom = "Full sense nom";
@@ -70,8 +68,8 @@ public class Full {
     //Mètodes Públics
 
     /**
-     * @brief Funció que afegeix una fila al full a l'índex on s'indica per paràmetre.
-     * @param nf
+     * Funcio que afegeix una fila al full a l'índex on s'indica per parametre.
+     * @param nf index de la fila
      */
     public void Afegir_Fila(Integer nf) throws Exception {
         int k= 0;
@@ -93,8 +91,8 @@ public class Full {
     };
 
     /**
-     * @brief Funció que afegeix una columna al full a l'índex on s'indica per paràmetre.
-     * @param nc
+     * Funcio que afegeix una columna al full a l'índex on s'indica per parametre.
+     * @param nc index de la columna
      */
     public void Afegir_Columna(Integer nc) throws Exception {
         int k= 0;
@@ -117,8 +115,8 @@ public class Full {
     };
 
     /**
-     * @brief Funció que elimina una fila al full a l'índex on s'indica per paràmetre.
-     * @param nf
+     * Funcio que elimina una fila al full a l'índex on s'indica per parametre.
+     * @param nf index de la fila
      */
     public void Eliminar_Fila(Integer nf) throws Exception {
         if (nf < this.Num_Files) DecrementarIndexFila(nf);
@@ -133,8 +131,8 @@ public class Full {
     };
 
     /**
-     * @brief Funció que elimina columna al full a l'índex on s'indica per paràmetre.
-     * @param nc
+     * Funcio que elimina columna al full a l'índex on s'indica per parametre.
+     * @param nc index de la columna
      */
     public void Eliminar_Columna(Integer nc) throws Exception{
         if (nc < this.Num_Columnes) DecrementarIndexCol(nc);
@@ -149,8 +147,8 @@ public class Full {
     };
 
     /**
-     * @brief Funció que esborra les celes que rep per paràmetre.
-     * @param celes
+     * Funcio que esborra les cel·les que rep per paràmetre.
+     * @param celes llista de cel·les per esborrar
      */
     public void Esborrar_Celes(ArrayList<Cela> celes) {
         int i= 0;
@@ -163,10 +161,10 @@ public class Full {
     };
 
     /**
-     * @brief Funció que modifica la cel·la "id" amb el resultat que es rep per paràmetre, comprovant el tipus de cel·la
-     * i comprovant si té obtervadors.
-     * @param id
-     * @param resultat
+     * Funcio que modifica la cel·la "id" amb el resultat que es rep per parametre, comprovant el tipus de cel·la
+     * i comprovant si te obtervadors.
+     * @param id identificador de la cel·la
+     * @param resultat nou resultat de la cel·la
      */
     public void Modifica_Cela(AbstractMap.SimpleEntry<Integer, Integer> id, String resultat) {
         int size = resultat.length();
@@ -247,16 +245,14 @@ public class Full {
                 this.Celes.get(id).setObservadors(observadors);
             }
         }
-        //AQUI HAY QUE ACTUALIZAR TODOS LOS OBSERVERS DE LA NUEVA CELDA SI ESTA CANVIA DE TIPO TAMBIEN HABRIA QUE TENERLO
-        //EN CUENTA
     }
 
     /**
-     * @brief Funció que avalua l'operació a realitzar amb els operadors "sender" i operació "oper",
+     * Funcio que avalua l'operacio a realitzar amb els operadors "sender" i operacio "oper",
      * i retorna el resultat.
-     * @param sender
-     * @param oper
-     * @return
+     * @param sender operadors
+     * @param oper operacio
+     * @return resultat de l'operacio
      */
     private BigDecimal avaluar(ArrayList<Numero> sender, String oper){
         if(Objects.equals(oper, "DIV")){
@@ -304,8 +300,8 @@ public class Full {
     }
 
     /**
-     * @brief Modifica el tipus de la cel·la amb id "id" pel tipus numeric.
-     * @param id
+     * Modifica el tipus de la cel·la amb id "id" pel tipus numeric.
+     * @param id identificador de la cel·la
      */
     public void Modifica_Tipus_Numeric(AbstractMap.SimpleEntry<Integer, Integer> id) {
         String resultat = this.Celes.get(id).getResultatFinal();
@@ -313,8 +309,8 @@ public class Full {
         this.Celes.get(id).setType("numeric");
     }
     /**
-     * @brief Modifica el tipus de la cel·la amb id "id" pel tipus text.
-     * @param id
+     * Modifica el tipus de la cel·la amb id "id" pel tipus text.
+     * @param id identificador de la cel·la
      */
     public void Modifica_Tipus_Text(AbstractMap.SimpleEntry<Integer, Integer> id) {
         String contingut = this.Celes.get(id).getResultatFinal();
@@ -322,8 +318,8 @@ public class Full {
         this.Celes.get(id).setType("text");
     }
     /**
-     * @brief Modifica el tipus de la cel·la amb id "id" pel tipus data.
-     * @param id
+     * Modifica el tipus de la cel·la amb id "id" pel tipus data.
+     * @param id identificador de la cel·la
      */
     public void Modifica_Tipus_Data(AbstractMap.SimpleEntry<Integer, Integer> id) {
         String contingut = this.Celes.get(id).getResultatFinal();
@@ -332,9 +328,9 @@ public class Full {
     }
 
     /**
-     * @brief Modifica el tipus de número de la cel·la amb id "id" pel tipus "type".
-     * @param id
-     * @param Type
+     * Modifica el tipus de numero de la cel·la amb id "id" pel tipus "type".
+     * @param id identificador de la cel·la
+     * @param Type nou tipus numero de la cel·la
      */
     public void Modifica_Numero_Tipus(AbstractMap.SimpleEntry<Integer, Integer> id, String Type) {
         Cela c = this.Celes.get(id);
@@ -353,8 +349,8 @@ public class Full {
     //Métodes Privats
 
     /**
-     * @brief Incrementa el índex de les files que tenen un índex superior a "nf".
-     * @param nf
+     * Incrementa el index de les files que tenen un index superior a "nf".
+     * @param nf index de la fila
      */
     private void IncrementarIndexFila(Integer nf) throws Exception{
         int i= Num_Files;
@@ -379,8 +375,8 @@ public class Full {
         }
     };
     /**
-     * @brief Incrementa el índex de les columnes que tenen un índex superior a "nc".
-     * @param nc
+     * Incrementa el index de les columnes que tenen un index superior a "nc".
+     * @param nc index de la columna
      */
     private void IncrementarIndexCol(Integer nc) throws Exception {
         int i= Num_Columnes;
@@ -406,8 +402,8 @@ public class Full {
     };
 
     /**
-     * @brief Decrementa el índex de les files que tenen un índex superior a "nf".
-     * @param nf
+     * Decrementa el index de les files que tenen un index superior a "nf".
+     * @param nf index de la fila
      */
     private void DecrementarIndexFila(Integer nf) throws Exception {
         int i= nf+1;
@@ -432,8 +428,8 @@ public class Full {
         }
     };
     /**
-     * @brief Decrementa el índex de les columnes que tenen un índex superior a "nc".
-     * @param nc
+     * Decrementa el index de les columnes que tenen un index superior a "nc".
+     * @param nc index de la columna
      */
     private void DecrementarIndexCol(Integer nc) throws Exception{
         int i= nc+1;
@@ -466,13 +462,6 @@ public class Full {
         return Num_Files;
     };
 
-    /**
-     * @brief Retorna una llista de cel·les tipus Numero a partir de "id1" i "id2" que delimiten el bloc
-     * de cel·les a tractar.
-     * @param id1
-     * @param id2
-     * @return
-     */
     public ArrayList<Numero> getBlocNumero(AbstractMap.SimpleEntry<Integer, Integer> id1, AbstractMap.SimpleEntry<Integer, Integer> id2) {
         ArrayList<AbstractMap.SimpleEntry<Integer, Integer>> ids = GetIdCeles(id1, id2);
         ArrayList<Numero> num = new ArrayList<>();
@@ -514,7 +503,7 @@ public class Full {
 
         for (AbstractMap.SimpleEntry<Integer, Integer> id : ids) {
             Cela c = this.Celes.get(id);
-                DataCela d = (DataCela) c;
+            DataCela d = (DataCela) c;
             data.add(d);
         }
 
@@ -591,12 +580,12 @@ public class Full {
     }
 
     /**
-     * @brief Ordena un bloc de cel·les acotat per les ids "id1" i "id2" segons indiqui el "criteri".
-     * @param id1
-     * @param id2
-     * @param cols
-     * @param criteri
-     * @throws Exception
+     * Ordena un bloc de cel·les acotat per les ids "id1" i "id2" segons indiqui el "criteri".
+     * @param id1 identificador de la primera cel·la del bloc
+     * @param id2 identificador de la ultima cel·la del bloc
+     * @param cols llista d'indexs de les columnes a tractar
+     * @param criteri criteri pel qual s'ordena el bloc
+     * @throws Exception si el bloc no es pot ordenar
      */
     public void ordena_bloc(AbstractMap.SimpleEntry<Integer, Integer> id1, AbstractMap.SimpleEntry<Integer, Integer> id2, ArrayList<Integer> cols, String criteri) throws Exception {
         ArrayList<Cela> C= getBlocCeles(id1,id2);
@@ -638,9 +627,9 @@ public class Full {
     }
 
     /**
-     * @brief Retorna cert si "strNum" és un número, fals si no ho és.
-     * @param strNum
-     * @return
+     * Retorna cert si "strNum" es un numero, fals si no ho es.
+     * @param strNum contingut de la cel·la
+     * @return retorna si es cert o fals
      */
     private Boolean isNumerical(String strNum){
         if (strNum == null) return false;
@@ -654,17 +643,17 @@ public class Full {
     }
 
     /**
-     * @brief Retorna cert si "id" existeix, fals si no.
-     * @param id
-     * @return
+     * Retorna cert si "id" existeix, fals si no.
+     * @param id identificador de la cel·la
+     * @return retorna si existeix
      */
     public boolean ExisteixId(AbstractMap.SimpleEntry<Integer, Integer> id) {
         return this.Celes.containsKey(id);
     }
 
     /**
-     * @brief Retorna la informació del full.
-     * @return
+     * Retorna la informacio del full.
+     * @return retorna tota la info i caracteristicas del full
      */
     @Override
     public String toString() {
@@ -677,8 +666,8 @@ public class Full {
     }
 
     /**
-     * @brief Mostra el full en forma de matriu de strings, formant-la i retornant-la.
-     * @return
+     * Mostra el full en forma de matriu de strings, formant-la i retornant-la.
+     * @return una matriu representant totes les cel·les del full
      */
     public String[][] Mostrar() { //El full hauria de retornar una ArrayList i així no haver de col·locar tot aixó al controller
         String[][] temp = new String[this.Num_Files][this.Num_Columnes];
@@ -711,9 +700,9 @@ public class Full {
     }
 
     /**
-     * @brief Retorna el resultat final, en forma de string, de la cel·la indicada.
-     * @param id
-     * @return
+     * Retorna el resultat final, en forma de string, de la cel·la indicada.
+     * @param id identificador de la cel·la
+     * @return retorna el seu resultat final
      */
     public String ValorTotal(AbstractMap.SimpleEntry<Integer, Integer> id) {
         Cela c = this.Celes.get(id);
@@ -745,10 +734,10 @@ public class Full {
     }
 
     /**
-     * @brief És la subfunció de modificacel·la que controla les referències de les cel·les.
-     * @param id
-     * @param l
-     * @param resultat
+     * Es la subfuncio de modificacel·la que controla les referencies de les cel·les.
+     * @param id identificador de la cel·la
+     * @param l llistat de cel·les
+     * @param resultat resultat del calcul
      */
     public void opera(AbstractMap.SimpleEntry<Integer, Integer> id, ArrayList<AbstractMap.SimpleEntry<Integer, Integer>> l, String resultat) {
 
@@ -825,9 +814,9 @@ public class Full {
             Celes.replace(id,aaux);
         }
 
-        if (l.remove(new AbstractMap.SimpleEntry<>(-2,-2)));
-    }
-else {
+            if (l.remove(new AbstractMap.SimpleEntry<>(-2,-2)));
+        }
+        else {
             ArrayList<Numero> aux = new ArrayList<>();
             for (int i = 0; i < l.size(); i++) {
                 if (Celes.get(l.get(i)) instanceof Numero) {
@@ -846,9 +835,9 @@ else {
     }
 
     /**
-     * @brief Esborra les referències de la cel·la amb id "id".
-     * @param id
-     * @param l
+     * Esborra les referencies de la cel·la amb id "id".
+     * @param id identificador de la cel·la
+     * @param l llistat de cel·les
      */
     public void borraref(AbstractMap.SimpleEntry<Integer, Integer> id, ArrayList<AbstractMap.SimpleEntry<Integer, Integer>> l) {
 
@@ -859,14 +848,14 @@ else {
     }
 
     /**
-     * @brief Funció que comprova si existeix algun bucle dins del bloc de cel·les comprés entre "id" i "id1".
-     * @param id
-     * @param id1
-     * @return
+     * Funcio que comprova si existeix algun bucle dins del bloc de cel·les compres entre "id" i "id1".
+     * @param id identificador de la primera cel·la del bloc
+     * @param id1 identificador de la ultima cel·la del bloc
+     * @return si es cert o fals
      */
     public boolean comprova_bucle(AbstractMap.SimpleEntry<Integer, Integer> id, AbstractMap.SimpleEntry<Integer, Integer> id1) {
-       Boolean b=false;
-       String res="";
+        Boolean b=false;
+        String res="";
         if( Celes.get(id1) instanceof CelaRefNum ) {
             res=((CelaRefNum) Celes.get(id1)).getContingut();
         }else if (Celes.get(id1) instanceof CelaRefText) {
@@ -885,10 +874,10 @@ else {
     }
 
     /**
-     * @brief Funció que modifica el contingut de la cel·la amb id "id", comprovant si no és referència de cap altra cel·la.
-     * @param id
-     * @param resultat
-     * @throws Exception
+     * Funcio que modifica el contingut de la cel·la amb id "id", comprovant si no és referencia de cap altra cel·la.
+     * @param id identificador de la cel·la
+     * @param resultat nou resultat de la cel·la
+     * @throws Exception si no s'ha pogut modificar la cel·la
      */
     public void modificarContingutCela(AbstractMap.SimpleEntry<Integer, Integer> id, String resultat) throws Exception {
 
@@ -956,9 +945,9 @@ else {
     }
 
     /**
-     * @brief Funció que comprova els observadors de la cel·la amb id "id".
-     * @param id
-     * @throws Exception
+     * Funcio que comprova els observadors de la cel·la amb id "id".
+     * @param id identificador de la cel·la
+     * @throws Exception sino s'ha pogut accedir a les seves observadores
      */
     public void CheckObs(AbstractMap.SimpleEntry<Integer, Integer> id) throws Exception {
         ArrayList<AbstractMap.SimpleEntry<Integer, Integer>> obs = getCeles().get(id).getObservadors();
@@ -984,11 +973,11 @@ else {
     }
 
     /**
-     * @brief Analitza el tipus d'operació de bloc que es vol realitzar.
-     * @param ops
-     * @param y
-     * @param x
-     * @return
+     * Funcio que analitza si la sintaxis de l'operacio que es vol realitzar es correcta
+     * @param ops contingut a analitzar
+     * @param y identificador de columna
+     * @param x identificador de fila
+     * @return si es correcta la sintaxis o no
      */
     public Boolean analitzablocs(String ops, Integer y, Integer x) {
         String tipus="";
@@ -1055,11 +1044,11 @@ else {
     }
 
     /**
-     * @brief Analitza y retorna el bloc de cel·les a operar.
-     * @param ops
-     * @param y
-     * @param x
-     * @return
+     * Funcio que analitza si la sintaxis de l'operacio que es vol realitzar i retorna la llista de cel·les que operen.
+     * @param ops contingut que analitza
+     * @param y index de les columnes
+     * @param x index de les files
+     * @return retorna la
      */
     private ArrayList<AbstractMap.SimpleEntry<Integer, Integer>> analitzablocs_ops(String ops, Integer y, Integer x) {
         String tipus="";
@@ -1115,6 +1104,3 @@ else {
         return l;
     }
 };
-
-
-
